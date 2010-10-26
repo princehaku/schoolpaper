5f6d811ff112bd30a6d4e444fdfbea4bf6a9aac0
baizhongwei@gmail.com <baizhongwei@gmail.com@a5f0bea8-32ee-7391-f8e7-62088d1646cb>
2010-10-26 23:47:54 +0000

    10月27号的版本 从这个版本开始创建版本库

--- src/com/techest/schoolpaper/MainActivity.java
+++ src/com/techest/schoolpaper/MainActivity.java
@@ -12,25 +12,131 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Created on : 2010-10-26, 21:31:26
+ *  Created on : 2010-10-18, 21:05:57
  *  Author     : princehaku
  */
-package org.me.testcanvas;
 
-import android.app.Activity;
+package com.techest.schoolpaper;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+import java.util.ArrayList;
 
 /**
  *
  * @author princehaku
  */
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity implements OnTouchListener{
 
+    private MapView map;
+    //测试用的文本框
+    private TextView mTextView01;
+    //信息点
+    private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+    //
+    private ImageView renderMapContainer;
+    //这个位图是缓冲图.存有地理信息
+    Bitmap renderMapCacheBitmap;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        setContentView(R.layout.mapview);
+        mTextView01 = (TextView) findViewById(R.id.TextView01);
+        //初始化圈圈的画布
+        renderMapContainer=(ImageView)findViewById(R.id.render_map);
+        renderMapContainer.setLongClickable(true);
+        renderMapContainer.setOnTouchListener((OnTouchListener)this);
+        //初始化地图
+        map = (MapView)findViewById(R.id.my_map);
+        map.getController().setCenter(new GeoPoint(30673894,104143757));
+        map.getController().setZoom(18);
+        map.setBuiltInZoomControls(true);
+        map.setLongClickable(true);
+        map.setFocusable(true);
+        map.setClickable(true);
+        //建立缓冲图像
+        int w = 320,h = 380;
+        renderMapCacheBitmap = Bitmap.createBitmap(w,h,Config.ARGB_8888);
+        //根据map虚拟化一张画布填充至画布层
+        map.setDrawingCacheEnabled(true);
+        renderMapContainer.setImageBitmap(map.getDrawingCache());
+        renderMapContainer.setVisibility(View.INVISIBLE);
+        //这里是圈圈的点击事件
+        ((Button)findViewById(R.id.quan)).setOnClickListener(
+
+         new OnClickListener(){
+
+            public void onClick(View v) {
+                //使画布显示 覆盖在mapview之上 开始工作
+                renderMapContainer.setVisibility(View.VISIBLE);
+            }
+            }
+        );
+        }
+        
+    /**必须重载此方法
+     *
+     * @return
+     */
+    @Override
+    protected boolean isRouteDisplayed() {
+       return false;
     }
 
+    /**这个方法是给地图层上的画图层 触摸的时候画线
+     * 离开的时候计算并发送信息给服务器
+     * @param v
+     * @param event
+     * @return
+     */
+    public boolean onTouch(View v, MotionEvent event) {
+        mTextView01.setText(event.getAction()+"-"+event.getPressure()+"");
+        //创建一个画布 给缓冲层使用
+        Canvas canvasTemp = new Canvas(renderMapCacheBitmap);
+        Paint p = new Paint();
+        p.setColor(Color.CYAN);
+        PointStatu.X=event.getX();
+        PointStatu.Y=event.getY();
+        switch(event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                PointStatu.lastX=PointStatu.X;
+                PointStatu.lastY=PointStatu.Y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //如果当前点的横或者纵的范围超过5则画图并设置到新点
+                if(Math.abs(PointStatu.Y-PointStatu.lastY)>5||Math.abs(PointStatu.X-PointStatu.lastX)>5){
+                    canvasTemp.drawLine(PointStatu.lastX,PointStatu.lastY, PointStatu.X,PointStatu.Y,p);
+                    PointStatu.lastX=PointStatu.X;
+                    PointStatu.lastY=PointStatu.Y;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                //释放的时候计算坐标成地理坐标.上传给服务器
+                GeoPoint p1= map.getProjection().fromPixels((int)PointStatu.X,(int)PointStatu.Y);
+                mTextView01.setText(p1.toString()+"-");
+                break;
+
+        }
+        //更新图像
+        renderMapContainer.setImageBitmap(renderMapCacheBitmap);
+        return false;
+    }
 
 }

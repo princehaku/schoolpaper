d5436ca1ddc2c3acd4ea15d6ef1915e105ffb6f8
baizhongwei@gmail.com <baizhongwei@gmail.com@a5f0bea8-32ee-7391-f8e7-62088d1646cb>
2010-11-04 02:29:28 +0000

    增加纸片显示模块

--- src/net/techest/schoolpaper/R.java
+++ src/net/techest/schoolpaper/R.java
@@ -16,14 +16,19 @@ public final class R {
     }
     public static final class id {
         public static final int TextView01=0x7f050004;
+        public static final int cc1=0x7f050007;
+        public static final int cc2=0x7f050008;
         public static final int my_map=0x7f050001;
         public static final int quan=0x7f050003;
         public static final int render_map=0x7f050002;
+        public static final int title=0x7f050006;
         public static final int tv=0x7f050000;
+        public static final int widget46=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int mapview=0x7f030001;
+        public static final int paperview=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;

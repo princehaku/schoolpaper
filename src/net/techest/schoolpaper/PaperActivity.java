a20bfc9fd49501941532731f690d41f96f9583d9
baizhongwei@gmail.com <baizhongwei@gmail.com@a5f0bea8-32ee-7391-f8e7-62088d1646cb>
2010-11-05 03:32:18 +0000

    增加点击图片的效果

--- src/net/techest/schoolpaper/PaperActivity.java
+++ src/net/techest/schoolpaper/PaperActivity.java
@@ -15,16 +15,22 @@
  *  Created on : 2010-11-4, 8:08:00
  *  Author     : princehaku
  */
-
 package net.techest.schoolpaper;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import net.techest.schoolpaper.paper.Paper;
 import net.techest.schoolpaper.util.XmlToPapers;
@@ -35,39 +41,82 @@ import net.techest.schoolpaper.util.XmlToPapers;
  */
 public class PaperActivity extends Activity {
     //警告窗口
+
     public AlertWindow alert;
     /**当前查看的纸片序号
      *
      */
-    private static int nowIndex=0;
+    private static int nowIndex = 0;
+    
+    private boolean showBigPic=false;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        alert=new AlertWindow(this);
+        alert = new AlertWindow(this);
         setContentView(R.layout.paperview);
         // ToDo add your GUI initialization code here
         setViewContent(nowIndex);
     }
-    private void setViewContent(int idx){
-        ArrayList<Paper> papers=PublicData.papers;
-        ((TextView)findViewById(R.id.cc2)).setScrollContainer(true);
-        ((TextView)findViewById(R.id.title)).setText(""+papers.get(idx).getTitle());
-        ((TextView)findViewById(R.id.date)).setText(""+papers.get(idx).getAddDate());
-        ((TextView)findViewById(R.id.cc2)).setText(papers.get(idx).getContent());
-        Log.v("","http://schoolpaper.techest.net/"+papers.get(idx).getImagePath());
-        ((ImageView)findViewById(R.id.thumb)).setImageURI(Uri.parse("http://schoolpaper.techest.net"+papers.get(idx).getImagePath()));
+
+    private void setViewContent(int idx) {
+        ArrayList<Paper> papers = PublicData.papers;
+        String imgurl = "http://schoolpaper.techest.net" + papers.get(idx).getImagePath();
+        ((TextView) findViewById(R.id.cc2)).setScrollContainer(true);
+        ((TextView) findViewById(R.id.title)).setText("" + papers.get(idx).getTitle());
+        ((TextView) findViewById(R.id.date)).setText("" + papers.get(idx).getAddDate());
+        ((TextView) findViewById(R.id.cc2)).setText(papers.get(idx).getContent());
+        Log.v("", "http://schoolpaper.techest.net/" + papers.get(idx).getImagePath());
+        ImageView image=((ImageView) findViewById(R.id.thumb));
+        image.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                showBigPic=true;
+                showBigPic();
+            }
+        });
     }
-    /**按下后退的时候返回到主界面
+
+    private void showBigPic() {
+        ArrayList<Paper> papers = PublicData.papers;
+        String imgurl = "http://schoolpaper.techest.net" + papers.get(nowIndex).getImagePath();
+        try {
+            URL url = new URL(imgurl);
+            URLConnection conn = url.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            Bitmap bm = BitmapFactory.decodeStream(is);
+            ImageView image = new ImageView(this);
+            image.setImageBitmap(bm);
+            image.setOnClickListener(new OnClickListener() {
+
+                public void onClick(View arg0) {
+                    setContentView(R.layout.paperview);
+                    setViewContent(nowIndex);
+                    showBigPic=false;
+                }
+            });
+            setContentView(image);
+            is.close();
+        } catch (Exception e) {
+            alert.show("失败", "读取图片失败..T T");
+        }
+    }
+
+    /**按下后退的时候的时间
      *
      */
     @Override
-    public void onPause(){
-      super.onPause();
-      Intent intent = new Intent();
-      intent.setClass(this, MainActivity.class);
-      this.startActivity(intent);
-      this.finish();
+    public void onPause() {
+        super.onPause();
+        if(this.showBigPic==true){
+            setContentView(R.layout.paperview);
+            setViewContent(nowIndex);
+            return;
+        }
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
+        this.startActivity(intent);
+        this.finish();
     }
-
 }
